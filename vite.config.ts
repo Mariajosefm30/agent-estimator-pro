eee1bece808be52d525e54bb579d8082022df184
Mariajosefm30 <mfernandezmelgar@gmail.com>
2026-02-13 11:34:20 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { componentTagger } from 'lovable-tagger'
 
 export default defineConfig(({ mode }) => ({
-  base: '/agent-estimator-pro/',
+  base: process.env.GITHUB_ACTIONS ? '/maf-quickstart/' : '/',
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
